0d3fe0529e7d1100b4703c029b37b4a623ee5509
Madness0105 <73240759+Madness0105@users.noreply.github.com>
2023-02-01 09:19:59 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Main } from './components/Main';
 import { Navbar } from './components/Navbar';
 
-export const SERVER_IP = 'playneptune.xyz';
-export const SERVER_DISCORD = 'https://discord.com';
+export const SERVER_IP = 'dylanspigot.men';
+export const SERVER_DISCORD = 'https://discord.dylanspigot.men';
 
 const darkTheme = createTheme({
     palette: {
